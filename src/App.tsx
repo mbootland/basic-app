91c6593a979e9a45b3ca66ccb1d8908cce6adcf2
Boots <matthew@playerdata.com>
2025-08-19 21:56:03 +0100

    fix: Squish me

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,34 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface LinkProps{
+  url: string;
 }
+
+function Link({ url }: LinkProps){
+  return (
+    <p>
+      <a href={url} target="_blank" rel="noopener noreferrer" className="App-link">
+        Learn more about React
+      </a>
+    </p>
+  );
+};
+
+function App(){
+  return (
+    <>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <Link url="https://react.dev/" />
+        </header>
+      </div>
+    </>
+  );
+};
 
 export default App;
